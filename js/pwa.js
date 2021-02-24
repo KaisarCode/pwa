3ba48906f6261f5ed81325780147f397e0ecf343
KaisarCode <kaisar@kaisarcode.com>
2021-02-23 23:37:51 -0300

    Only PWA icons

--- js/pwa.js
+++ js/pwa.js
@@ -1,28 +1,3 @@
-(function(){
-    var evt = null;
-    
-    // Register service worker
-    if ('serviceWorker' in navigator){
-        navigator.serviceWorker.register('/sw.js')
-    }
-    
-    // Check if pwa is installed
-    window.addEventListener('appinstalled', function() {
-        //installedPWA();
-    });
-    
-    // Custom install pwa
-    window.addEventListener('beforeinstallprompt', function(e) {
-        e.preventDefault();
-        var evt = e;
-    });
-    
-    document.querySelectorAll('.pwa-install').forEach(function(btn){
-        btn.addEventListener('click', function() {
-            if (!evt) return;
-            evt.prompt();
-            evt = null;
-        });
-    });
-    
-})();
+if ('serviceWorker' in navigator){
+    navigator.serviceWorker.register('js/sw.js')
+}
