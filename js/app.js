f76ae9ec46f2d429fe9023840d2a642ac7b9f311
KaisarCode <kaisar@kaisarcode.com>
2021-03-15 07:29:35 -0300

    add deferred install

--- js/app.js
+++ js/app.js
@@ -5,3 +5,31 @@ if ('serviceWorker' in navigator) {
         .register('worker.js', { scope:'.' });
     });
 }
+
+// Install
+(function(){
+    var btn = document.getElementById('install');
+    
+    // Defer prompt
+    window.addEventListener('beforeinstallprompt', (event) => {
+        window.deferredPrompt = event;
+        btn.classList.toggle('hidden', false);
+    });
+    
+    // Call prompt
+    btn.addEventListener('click', async () => {
+        const promptEvent = window.deferredPrompt;
+        if (!promptEvent) return;
+        promptEvent.prompt();
+        const result = await promptEvent.userChoice;
+        window.deferredPrompt = null;
+        btn.classList.toggle('hidden', true);
+    });
+    
+    // Clear prompt
+    window.addEventListener('appinstalled', (event) => {
+        window.deferredPrompt = null;
+    });
+})();
+
+
