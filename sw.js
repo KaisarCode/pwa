b2473ddd9d7d1f8757e585b3f5462f2446e50348
KaisarCode <kaisar@kaisarcode.com>
2021-03-16 23:56:03 -0300

    Add 2 cache layers

--- sw.js
+++ sw.js
@@ -1,26 +1,24 @@
-const cache_name = 'v1';
+const cache_name = 'files';
 
-// Install
+// Cache 1
+let files = [
+    './',
+    './css/pwa.css',
+    './manifest.json',
+    './js/sw-load.js',
+    './js/pwa-install.js',
+    './img/icon/favicon.ico',
+    './img/icon/icon-192x192.png',
+];
 self.addEventListener('install', e => {
-});
-
-// Activate
-self.addEventListener('activate', e => {
     e.waitUntil(
-        caches.keys().then(cache_names => {
-            return Promise.all(
-                cache_names.map(cache => {
-                    if (cache !== cache_name) {
-                        // Clear old cache
-                        return caches.delete(cache);
-                    }
-                })
-            );
+        caches.open(cache_name).then(cache => {
+            return cache.addAll(files);
         })
     );
 });
 
-// Fetch
+// Cache 2
 self.addEventListener('fetch', e => {
     e.respondWith(
         fetch(e.request).then(res => {
@@ -28,13 +26,8 @@ self.addEventListener('fetch', e => {
             const res_clone = res.clone();
             caches.open(cache_name).then(cache => {
                 var url = e.request.url;
-                // Filter files
-                // ... all by default
-                
-                // Add to cache
                 cache.put(url, res_clone);
-            });
-            return res;
+            }); return res;
         }).catch(err => caches.match(e.request).then(res => res))
     );
 });
